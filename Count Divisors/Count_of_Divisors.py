17566185ab5c2672897e4ab52072a036f7bb1015
Dharun <50405830+Dharun-A@users.noreply.github.com>
2019-05-15 18:18:27 +0530

    Update Count_of_Divisors.py

--- Count Divisors/Count_of_Divisors.py
+++ Count Divisors/Count_of_Divisors.py
@@ -1,8 +1,10 @@
+//print("\nCount Divisors\nEnter the starting,ending and number :\n")
 l, r, k = map(int, input().split(" "))
 c = 0
 
 for i in range(l,r+1):
   if i%k == 0:
     c += 1
-
+    
+//print("\nThe count of divisors is \n")
 print(c)
